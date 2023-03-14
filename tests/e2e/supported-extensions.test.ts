6d012e3ab949ad7f0409aa48b8b91b7fb4795198
1x2kb <1x2kb@protonmail.com>
2023-03-14 17:34:04 -0500

    Added supported extensions

---
+++ tests/e2e/supported-extensions.test.ts
@@ -0,0 +1,21 @@
+import { beforeAll, describe, expect, it } from 'vitest';
+import { UnstableDevWorker, unstable_dev } from 'wrangler';
+
+describe('supported-extension should', () => {
+  let worker: UnstableDevWorker;
+
+  beforeAll(async () => {
+    worker = await unstable_dev('src/index.ts', {
+      experimental: { disableExperimentalWarning: true },
+    });
+  });
+
+  it('return supported extensions', async () => {
+    const resp = await worker.fetch('/api/v1/extensions');
+
+    if (resp) {
+      const response = await resp.json();
+      expect(response).toEqual(['.eth', '.avax']);
+    }
+  });
+});
